b1745050de98c9c3eddc468d3e6949b6ae6d7781
Jacob MacFarland <macfarlandjw@appstate.edu>
2020-01-15 00:40:28 -0500

    Added transaction print formatting and summary for how much coffee bought in last 500 transactions

--- TestPlaid.py
+++ TestPlaid.py
@@ -3,24 +3,44 @@ from pprint import pprint
 from typing import List
 from plaid import Client as PlaidClient
 
-
 plaid_client = PlaidClient(client_id=os.getenv('PLAID_CLIENT_ID'),
 						   secret=os.getenv('PLAID_SECRET'),
 			   			   public_key=os.getenv('PLAID_PUBLIC_KEY'),
 			   			   environment=os.getenv('PLAID_ENV'))
 
 def get_some_transactions(access_token: str, start_date: str, end_date: str) -> List[dict]:
-	return plaid_client.Transactions.get(access_token, start_date, end_date)
+	return plaid_client.Transactions.get(access_token, start_date, end_date, count=500)
 
 some_transactions = get_some_transactions(os.getenv('WELLS_ACCESS_TOKEN'), '1972-01-01', '2020-05-26')
 
 print(f'there are {some_transactions["total_transactions"]} total transactions between those dates.')
 print(f'get_some_transactions returned {len(some_transactions["transactions"])} transactions.')
 
-# for transaction in some_transactions['transactions']:
-#	print(transaction)
+totalCoffeeTransactionsAmount = 0.0
+
+for transaction in some_transactions['transactions']:
 	
-print(some_transactions['transactions'][0])	
+	name = transaction["name"][0:30]
+	amountFloat = transaction["amount"]
+	amount = str(amountFloat)
+	date = transaction["date"]
+	types = ""
+	
+	for t in transaction["category"]:
+		types = types + " " + t + " -"
+
+	#print("Store: {0:40} Amount: {1:15} Date: {2:15} Type: {3:10}".format(name, amount, date, types))
+	
+	
+	# Format strings to mine data for coffee shops
+	types = types.lower()
+	name = name.lower()
+	
+	if "local lion" in name or "espresso" in name or "coffee" in types or "coffee" in name:
+		totalCoffeeTransactionsAmount += amountFloat
+			
+print("Total money spent on coffee: ${:.2f}".format(totalCoffeeTransactionsAmount))
+#print(some_transactions['transactions'][0])	
 
 # pprint(some_transactions['transactions'][0].keys())
 
