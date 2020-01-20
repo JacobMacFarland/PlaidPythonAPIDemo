2d6810e371a2e702f0ebec15c3011e8387b2933a
Jacob MacFarland <macfarlandjw@appstate.edu>
2020-01-20 14:14:35 -0500

    Added multiple bank account transactions and renaming of variables

--- TestPlaid.py
+++ TestPlaid.py
@@ -3,22 +3,35 @@ from pprint import pprint
 from typing import List
 from plaid import Client as PlaidClient
 
+
 plaid_client = PlaidClient(client_id=os.getenv('PLAID_CLIENT_ID'),
 						   secret=os.getenv('PLAID_SECRET'),
 			   			   public_key=os.getenv('PLAID_PUBLIC_KEY'),
 			   			   environment=os.getenv('PLAID_ENV'))
 
-def get_some_transactions(access_token: str, start_date: str, end_date: str) -> List[dict]:
-	return plaid_client.Transactions.get(access_token, start_date, end_date, count=500)
+def get_bank_transactions(access_token: str, start_date: str, end_date: str) -> List[dict]:
+	return plaid_client.Transactions.get(access_token, start_date, end_date, count = 500)
+	
+#def get_bank_balance(access_token: str):
+#	return plaid_client.Accounts.Balance.get(access_token)
 
-some_transactions = get_some_transactions(os.getenv('WELLS_ACCESS_TOKEN'), '1972-01-01', '2020-05-26')
 
-print(f'there are {some_transactions["total_transactions"]} total transactions between those dates.')
-print(f'get_some_transactions returned {len(some_transactions["transactions"])} transactions.')
+
+
+wells_bank_transactions = get_bank_transactions(os.getenv('WELLS_ACCESS_TOKEN'), '1972-01-01', '2020-05-26')
+
+coastal_bank_transactions = get_bank_transactions(os.getenv("COASTAL_ACCESS_TOKEN"), '1972-01-01', '2020-05-26')
+# bank_balance = get_bank_balance(os.getenv('WELLS_ACCESS_TOKEN'))
+
+print(f'Wells Fargo: there are {wells_bank_transactions["total_transactions"]} total transactions between those dates.')
+print(f'Wells Fargo: get_wells_bank_transactions returned {len(wells_bank_transactions["transactions"])} transactions.')
+
+print(f'Coastal: there are {coastal_bank_transactions["total_transactions"]} total transactions between those dates.')
+print(f'Coastal: returned {len(coastal_bank_transactions["transactions"])} transactions.')
 
 totalCoffeeTransactionsAmount = 0.0
 
-for transaction in some_transactions['transactions']:
+for transaction in wells_bank_transactions['transactions']:
 	
 	name = transaction["name"][0:30]
 	amountFloat = transaction["amount"]
@@ -29,7 +42,7 @@ for transaction in some_transactions['transactions']:
 	for t in transaction["category"]:
 		types = types + " " + t + " -"
 
-	#print("Store: {0:40} Amount: {1:15} Date: {2:15} Type: {3:10}".format(name, amount, date, types))
+	print("Store: {0:40} Amount: {1:15} Date: {2:15} Type: {3:10}".format(name, amount, date, types))
 	
 	
 	# Format strings to mine data for coffee shops
@@ -40,11 +53,3 @@ for transaction in some_transactions['transactions']:
 		totalCoffeeTransactionsAmount += amountFloat
 			
 print("Total money spent on coffee: ${:.2f}".format(totalCoffeeTransactionsAmount))
-#print(some_transactions['transactions'][0])	
-
-# pprint(some_transactions['transactions'][0].keys())
-
-#print({category
-#       for transaction in some_transactions['transactions'] if transaction['category'] is not None
-#       for category in transaction['category']})
-#pprint(some_transactions['accounts'])
